71ad8e323102a53a34c109d10859091f51d102f0
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-20 23:14:52 +0200

    minor tweaks

--- lyriks/core/video_generator.py
+++ lyriks/core/video_generator.py
@@ -1,5 +1,4 @@
 import click
-import numpy as np
 from moviepy import *
 from pathlib import Path
 from matplotlib import font_manager
@@ -36,9 +35,10 @@ class VideoGenerator:
         txt_clip = txt_clip.with_position("center").with_start(start).with_end(end)
         self.text_clips.append(txt_clip)
         
-    def render_video(self, output):
+    def render_video(self, output_file_name):
         self.video = CompositeVideoClip([self.clip] + self.text_clips)
         self.video = self.video.with_audio(self.audio)
+        output = output_file_name + ".mp4"
         self.video.write_videofile(
             output,
             temp_audiofile="temp-audio.mp3",
