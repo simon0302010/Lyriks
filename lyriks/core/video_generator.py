7f2fe8fdc8c958b7e42e358edeae351971ad4ccc
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-20 18:09:12 +0200

    add support for adding multiple text objects

--- lyriks/core/video_generator.py
+++ lyriks/core/video_generator.py
@@ -8,13 +8,13 @@ class VideoGenerator:
         if clip_path is None:
             clip_path = Path(__file__).parent.parent / "templates" / "video.mp4"
             clip_path = clip_path.resolve()
-        self.clip = VideoFileClip(clip_path).subclipped(0, 20)
+        self.video = (VideoFileClip(str(clip_path)).subclipped(0, 20))
         click.secho("Video Generator initialized", fg="green")
         
     def add_text(self, text):
-        self.txt_clip = TextClip(text=text, font_size=70, color="white")
-        self.txt_clip = self.txt_clip.with_position("center").with_duration(10)
+        txt_clip = TextClip(text=text, font_size=70, color="white")
+        txt_clip = txt_clip.with_position("center").with_duration(10)
+        self.video = CompositeVideoClip([self.video, txt_clip])
         
     def render_video(self, output):
-        self.video = CompositeVideoClip([self.clip, self.txt_clip])
         self.video.write_videofile(output)
