a338969d913a0aca5e57ef4ce21293898613b8ac
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-20 22:17:45 +0200

    added audio

--- lyriks/core/video_generator.py
+++ lyriks/core/video_generator.py
@@ -5,12 +5,16 @@ from pathlib import Path
 from matplotlib import font_manager
 
 class VideoGenerator:
-    def __init__(self, clip_path=None):
+    def __init__(self, audio_path, clip_path=None):
+        self.audio = AudioFileClip(str(audio_path))
+        self.audio_duration = self.audio.duration
+        self.audio = self.audio.subclipped(0, 20)
         if clip_path is None:
             clip_path = Path(__file__).parent.parent / "templates" / "video.mp4"
             clip_path = clip_path.resolve()
         self.clip = (VideoFileClip(str(clip_path)).subclipped(0, 20))
-        self.video_height, self.video_width = self.clip.h, self.clip.w
+        self.video_width = self.clip.w
+        self.video_height = self.clip.h
         self.font_path = font_manager.findfont("DejaVu Sans")
         self.text_clips = []
         click.secho("Video Generator initialized", fg="green")
@@ -29,9 +33,11 @@ class VideoGenerator:
         
     def render_video(self, output):
         self.video = CompositeVideoClip([self.clip] + self.text_clips)
+        self.video = self.video.with_audio(self.audio)
         self.video.write_videofile(
             output,
-            temp_audiofile="temp-audio.m4a",
+            temp_audiofile="temp-audio.mp3",
             remove_temp=True,
-            codec="libx264"
+            codec="libx264",
+            audio_codec="libmp3lame"
         )
